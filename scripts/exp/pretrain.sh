8a1b8ae7b68cd64d9a158c70f954b2d008ceaa0b
tanhuajie <tanhuajie264@163.com>
2024-10-23 03:15:24 +0800

    update training scripts

--- scripts/exp/pretrain.sh
+++ scripts/exp/pretrain.sh
@@ -1,8 +1,8 @@
 ############### Pretrain ################
 
-export BASE_RUN_NAME=mm-projection_Qwen2.5-7B-Instruct_siglip-so400m-patch14-384
+export BASE_RUN_NAME=mm-projection_Qwen2-7B-Instruct_siglip-so400m-patch14-384
 
-export LLM_VERSION=/home/vlm/pretrain_model/Qwen2.5-7B-Instruct
+export LLM_VERSION=/home/vlm/pretrain_model/Qwen2-7B-Instruct
 export VISION_MODEL_VERSION=/home/vlm/pretrain_model/siglip-so400m-patch14-384
 
 export DATA_PATH=/home/vlm/pretrain_json/blip_laion_cc_sbu_558k.json
@@ -14,7 +14,8 @@ export PROMPT_VERSION=plain
 export MM_TUNABLE_PARTS="mm_mlp_adapter"
 
 export NUM_GPUS=8
-export NNODES=1
+export NNODES=4
+export HOSTFILE=/home/vlm/workspace/hostfile/hostfile_group2_4
 
 export DATA_WORKERS=4
 export DEV_BATCHSIZE=4
@@ -24,7 +25,11 @@ export LEARNING_RATE=1e-3
 export MAX_SEQ_LEN=4096
 export ZERO_VERSION=2
 
-deepspeed --num_gpus $NUM_GPUS --num_nodes $NNODES \
+if [ ! -d "$OUTPUT_DIR" ]; then
+  mkdir "$OUTPUT_DIR"
+fi
+
+deepspeed --num_gpus $NUM_GPUS --num_nodes $NNODES --hostfile $HOSTFILE \
     llava/train/train_mem.py \
     --deepspeed scripts/zero${ZERO_VERSION}.json \
     --model_name_or_path $LLM_VERSION \
@@ -33,13 +38,13 @@ deepspeed --num_gpus $NUM_GPUS --num_nodes $NNODES \
     --image_folder $IMAGE_FOLDER \
     --vision_tower $VISION_MODEL_VERSION \
     --mm_tunable_parts $MM_TUNABLE_PARTS \
-    --output_dir $OUTPUT_DIR \    
+    --output_dir $OUTPUT_DIR \
     --per_device_eval_batch_size $DEV_BATCHSIZE \
     --per_device_train_batch_size $DEV_BATCHSIZE \
     --gradient_accumulation_steps $GRAD_ACC_STEPS \
     --model_max_length $MAX_SEQ_LEN \
     --dataloader_num_workers $DATA_WORKERS \
-    --learning_rate $LEARNING_RATE \    
+    --learning_rate $LEARNING_RATE \
     --mm_vision_select_layer -2 \
     --mm_projector_type mlp2x_gelu \
     --mm_use_im_start_end False \
@@ -59,6 +64,6 @@ deepspeed --num_gpus $NUM_GPUS --num_nodes $NNODES \
     --report_to wandb \
     --run_name $BASE_RUN_NAME \
     --attn_implementation sdpa \
-    2>&1 | tee -a $OUTPUT_DIR/train.log
+    2>&1 | tee $OUTPUT_DIR/train.log
 
 # You can delete the sdpa attn_implementation if you want to use flash attn
